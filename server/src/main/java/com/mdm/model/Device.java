45e209a00fc5aa50c108b8c7e4afdeb5bdbd0629
benray95 <148528492+benray95@users.noreply.github.com>
2024-11-23 12:37:05 +0100

    Add files via upload

--- server/src/main/java/com/mdm/model/Device.java
+++ server/src/main/java/com/mdm/model/Device.java
@@ -23,66 +23,11 @@ public class Device {
     @Enumerated(EnumType.STRING)
     private DeviceStatus status;
 
-    // Getters et Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getImei() {
-        return imei;
-    }
-
-    public void setImei(String imei) {
-        this.imei = imei;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public String getOsVersion() {
-        return osVersion;
-    }
-
-    public void setOsVersion(String osVersion) {
-        this.osVersion = osVersion;
-    }
-
-    public LocalDateTime getLastCheckin() {
-        return lastCheckin;
-    }
-
-    public void setLastCheckin(LocalDateTime lastCheckin) {
-        this.lastCheckin = lastCheckin;
-    }
-
-    public DeviceStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(DeviceStatus status) {
-        this.status = status;
-    }
-
     public enum DeviceStatus {
         ACTIVE,
         INACTIVE,
         LOST
     }
+
+    // Getters et Setters
 }

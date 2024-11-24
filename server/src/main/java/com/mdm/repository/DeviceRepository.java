8f968297f2ed1349879aee81a099219b624228e9
benray95 <148528492+benray95@users.noreply.github.com>
2024-11-24 13:38:47 +0100

    Delete server directory

--- server/src/main/java/com/mdm/repository/DeviceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mdm.repository;
-
-import com.mdm.model.Device;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeviceRepository extends JpaRepository<Device, Long> {
-}
